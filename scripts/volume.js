c682586aa33de5ec6885e5afecf87eab3105f0f3
Confettimaker <jeajohnadams@gmail.com>
2025-03-22 16:41:32 -0500

    Completed naive way of gaining full functionality

--- scripts/volume.js
+++ scripts/volume.js
@@ -1,29 +1,41 @@
-let volume = document.createElement("input");
-volume.type = "range";
-volume.id = "volume-slider";
+url = window.location.toString();
+baseURL = "https://replay.pokemonshowdown.com/";
 
-vol = localStorage.getItem("replayVolume");
-if (vol)
+if (url == "https://replay.pokemonshowdown.com/" || url.indexOf("=") > 0) // Change how battle links work so I can get the slider to load
 {
-    volume.setAttribute("value", vol);
+    getLinksCode = "let links = document.getElementsByTagName('a');";
+    forLoopCode = "for ( let link = 0; link < links.length; link ++) { href = links[link].href; if (!href.includes('javascript')) { newHref = 'javascript:window.location=`' + href + '`'; links[link].setAttribute('href', newHref); } }";
+    document.getElementById("main").setAttribute("onmouseover", getLinksCode + forLoopCode);
 }
 else
 {
-    vol = 5;
-    localStorage.setItem("replayVolume", vol);
-    volume.setAttribute("value", vol);
+    let volume = document.createElement("input");
+    volume.type = "range";
+    volume.id = "volume-slider";
+    
+    vol = localStorage.getItem("replayVolume");
+    if (vol)
+    {
+        volume.setAttribute("value", vol);
+    }
+    else
+    {
+        vol = 5;
+        localStorage.setItem("replayVolume", vol);
+        volume.setAttribute("value", vol);
+    }
+    
+    setEffectVolumeCode = "BattleSound.setEffectVolume(localStorage.getItem('replayVolume')); ";
+    setBgmVolumeCode = "BattleSound.setBgmVolume(localStorage.getItem('replayVolume')); "
+    removeAttributeCode = "document.getElementById('main').removeAttribute('onmouseover')";
+    document.getElementById("main").setAttribute("onmouseover", setEffectVolumeCode + setBgmVolumeCode + removeAttributeCode);
+    
+    setEffectVolumeCode = "BattleSound.setEffectVolume(document.getElementById('volume-slider').value); ";
+    setBgmVolumeCode = "BattleSound.setBgmVolume(document.getElementById('volume-slider').value); ";
+    setMuteUnMuteCode = "BattleSound.setMute(true); BattleSound.setMute(false);";
+    setLocalStorageCode = "localStorage.setItem('replayVolume', document.getElementById('volume-slider').value)";
+    volume.setAttribute("onchange", setEffectVolumeCode + setBgmVolumeCode + setMuteUnMuteCode + setLocalStorageCode);
+    
+    
+    document.getElementsByClassName("replay-controls")[0].appendChild(volume);
 }
-
-setEffectVolumeCode = "BattleSound.setEffectVolume(localStorage.getItem('replayVolume')); ";
-setBgmVolumeCode = "BattleSound.setBgmVolume(localStorage.getItem('replayVolume')); "
-removeAttributeCode = "document.getElementById('main').removeAttribute('onmouseover')";
-document.getElementById("main").setAttribute("onmouseover", setEffectVolumeCode + setBgmVolumeCode + removeAttributeCode);
-
-setEffectVolumeCode = "BattleSound.setEffectVolume(document.getElementById('volume-slider').value); ";
-setBgmVolumeCode = "BattleSound.setBgmVolume(document.getElementById('volume-slider').value); ";
-setMuteUnMuteCode = "BattleSound.setMute(true); BattleSound.setMute(false);";
-setLocalStorageCode = "localStorage.setItem('replayVolume', document.getElementById('volume-slider').value)";
-volume.setAttribute("onchange", setEffectVolumeCode + setBgmVolumeCode + setMuteUnMuteCode + setLocalStorageCode);
-
-
-document.getElementsByClassName("replay-controls")[0].appendChild(volume);
